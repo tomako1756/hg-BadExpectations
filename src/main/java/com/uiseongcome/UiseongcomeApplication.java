04941680e3e8daffe8097b2fa53110d563296c95
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-08-25 23:33:36 +0900

    init

---
+++ src/main/java/com/uiseongcome/UiseongcomeApplication.java
@@ -0,0 +1,13 @@
+package com.uiseongcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiseongcomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiseongcomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uiseongcome/UiseongcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uiseongcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiseongcomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
